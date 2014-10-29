44863b385b070fba6ce9c0ba17f9b7ac393be521
denis.boginich <denis.boginich@gmail.com>
2014-10-29 22:53:26 +0600

    Поправил метод. Исправил точность.

--- FirstHomeTask/CIntegral/CIntegral/EntryPoint.cpp
+++ FirstHomeTask/CIntegral/CIntegral/EntryPoint.cpp
@@ -2,53 +2,59 @@
 #include <omp.h>
 #include <mpi.h>
 
-static int processes_cnt = 16;
-static int threads_cnt = 8;
+static int threads_cnt = 4;
 
 static double the_func(double x)
 {
 	return (x - 3) * (x - 1) * (x + 1) / 5;
 }
 
-static double rectangel_method(double start, double dx, int startIndex, int endIndex) {
+static double rectangel_method(double start, double dx, long long int startIndex, long long int endIndex) {
 	double sum = 0;
 	double curX;
 #pragma omp parallel for num_threads (threads_cnt) reduction (+:sum) private (curX)
-	printf("%d - %d\n", startIndex, endIndex);
-	for (int i = startIndex; i <= endIndex; ++i) {
+	printf("%lld - %lld\n", startIndex, endIndex);
+	for (long long int i = startIndex; i <= endIndex; ++i) {
 		curX = start + i * dx;
 		sum += the_func(curX);
 	}
-	return sum * dx;
+	return sum;
 }
 
 int main(int argc, char* argv[]) {
+	double ans = 0;
+
 	double start = -3;
 	double end = 4;
-	int gran = 100000000; //100M
+	long long int gran = 10000000000; //10000M
 	double dx = (end - start) / gran;	
 
-	int startIndex, endIndex;
+	long long int startIndex, endIndex;
 	int rank, size;
 	
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	
-	int myGran = gran / size;
-	int commonMod = gran % size;
+	long long int myGran = gran / size;
+	long long int commonMod = gran % size;
 
 	int sd = commonMod > rank ? rank : commonMod;
 	startIndex = myGran * rank + sd;
 
 	int fd = commonMod > rank ? 1 : 0;
-	int isLast = size - 1 == rank ? 2 : 0;
-	endIndex = startIndex + myGran + fd - 1 + isLast;
+	endIndex = startIndex + myGran + fd - 1;
 
 	double res = rectangel_method(start, dx, startIndex, endIndex);
-	printf("%.9lf\n", res);
+	//printf("%.9lf\n", res);
+	
+	MPI_Reduce(&res, &ans, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	
+	if(rank == 0)
+		printf("Final result is %.12lf.\n", ans * dx);
 	
 	MPI_Finalize();
 	
+
 	return 0;
 }
