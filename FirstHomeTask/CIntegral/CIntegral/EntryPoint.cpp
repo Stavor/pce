de0d6c8e5af6fb5e1f77e88336916d6022a713c8
denis.boginich <denis.boginich@gmail.com>
2014-10-29 20:19:32 +0600

    MPICH. Подключил MPICH.

--- FirstHomeTask/CIntegral/CIntegral/EntryPoint.cpp
+++ FirstHomeTask/CIntegral/CIntegral/EntryPoint.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <omp.h>
+#include <mpi.h>
 
-static int threads_cnt = 16;
+static int processes_cnt = 16;
+static int threads_cnt = 8;
 
 static double the_func(double x)
 {
@@ -12,23 +14,29 @@ static double rectangel_method(double start, double dx, int cnt) {
 	double sum = 0;
 	double curX;
 #pragma omp parallel for num_threads (threads_cnt) reduction (+:sum) private (curX)
-	for (intptr_t i = 0; i < cnt; ++i) {
+	for (int i = 0; i < cnt; ++i) {
 		curX = start + i * dx;
 		sum += the_func(curX);
 	}
 	return sum * dx;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
 	double start = -3;
 	double end = 4;
 	int prec = 100000000;
 	double dx = (end - start) / prec;
-
-	double st = omp_get_wtime();
-	double res = rectangel_method(start, dx, prec + 1);
-	double fn = omp_get_wtime();
 	
-	printf("%.9lf / time: %.6lf\n", res, fn - st);
+	int rank, size;
+	
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	
+	double res = rectangel_method(start, dx, prec + 1);
+	printf("%.9lf\n", res);
+	
+	MPI_Finalize();
+	
 	return 0;
 }
