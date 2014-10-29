a5d26c9b53633d3daf7698bc1bf2625eead10887
denis boginich <denis.boginich@skbkontur.ru>
2014-10-29 17:09:13 +0500

    OpenMP сумма элементов массива

--- FirstHomeTask/CIntegral/CIntegral/EntryPoint.cpp
+++ FirstHomeTask/CIntegral/CIntegral/EntryPoint.cpp
@@ -1,19 +1,22 @@
 #include <stdio.h>
-#include <windows.h>
 #include <omp.h>
 
-static int threads_cnt = 7;
+static int threads_cnt = 16;
+static __int64 A[10000005];
+static int _10M = 10000000;
+
+__int64 static_array_sum() {
+	__int64 sum = 0;
+#pragma omp parallel for num_threads (threads_cnt) reduction(+:sum)
+	for (intptr_t i = 0; i < _10M; i++)
+		sum += A[i]; 
+	return sum;
+}
 
 int main() {
-	printf("Process num: %d\n\n", omp_get_num_procs());
-	int threadId;
-#pragma omp parallel num_threads (threads_cnt) firstprivate (threadId)
-	{
-		threadId = omp_get_thread_num();
-		if(threadId % 2 == 0)
-			Sleep(2000);
-		printf("%d: Hello World!\n", threadId);
-	}
-	printf("The End\n");
+	for (intptr_t i = 0; i < _10M; i++)
+		A[i] = i;
+	__int64 res = static_array_sum();
+	printf("%I64d\n", res); //correct res is 49999995000000
 	return 0;
 }
